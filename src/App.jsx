dc9959929451c1c4af34432d355e01ec66a2f8b6
GunwooJo <kanggi1997@naver.com>
2024-11-14 15:06:05 +0900

    Feat: 컴포넌트 별 url 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MemberJoin from './pages/MemberJoin';
+import Home from "./pages/Home.jsx";
+import AlertSetting from "./pages/AlertSetting.jsx";
+import MemberLogin from "./pages/MemberLogin.jsx";
+import AddAlert from "./pages/AddAlert.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/member/join" element={<MemberJoin/>} />
+        <Route path="/member/login" element={<MemberLogin/>} />
+        <Route path="/alerts" element={<AlertSetting/>} />
+        <Route path="/alerts/add" element={<AddAlert/>} />
       </Routes>
     </Router>
   );
